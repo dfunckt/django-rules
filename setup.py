a5e5f416bf9fd3875f329436bb052b6731806841
Akis Kesoglou <akiskesoglou@gmail.com>
2014-06-09 19:31:07 +0300

    We're also compatible with Python 3.4

--- setup.py
+++ setup.py
@@ -71,5 +71,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
