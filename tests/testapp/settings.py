3d66fff292cebfab581ad684fe66f8ba274893dc
orf <orf@users.noreply.github.com>
2016-12-10 14:48:42 +0000

    Update settings.py

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -58,21 +58,3 @@ TEMPLATES = [
         },
     },
 ]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django-rules': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
