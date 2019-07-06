b8dc4989a59c14c3b8777b3d5d18111b8d1ccb1b
Tobias Kunze <r@rixx.de>
2019-07-06 11:16:55 +0200

    Add shim for six for compatibility
    Django 3.0 will drop support for Python 2 and remove its vendored
    version of six. Closes #96, closes #98.

---
+++ rules/compat/six.py
@@ -0,0 +1,19 @@
+import functools
+import sys
+
+
+if sys.version_info[0] == 3:
+    string_types = str,
+else:
+    string_types = basestring,
+
+if sys.version_info[0:2] < (3, 4):
+    def wraps(wrapped, assigned=functools.WRAPPER_ASSIGNMENTS,
+              updated=functools.WRAPPER_UPDATES):
+        def wrapper(f):
+            f = functools.wraps(wrapped, assigned, updated)(f)
+            f.__wrapped__ = wrapped
+            return f
+        return wrapper
+else:
+    wraps = functools.wraps
