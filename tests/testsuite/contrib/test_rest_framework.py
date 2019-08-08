e35c5a5bc0985940f48a0ccdc3af3e1ec2874fa7
Robert Schindler <r.schindler@efficiosoft.com>
2019-08-08 12:17:57 +0200

    Add mixins for CBV and DRF that check permissions automatically

---
+++ tests/testsuite/contrib/test_rest_framework.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import
+
+import sys
+import unittest
+
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import ImproperlyConfigured
+from django import http
+from django.test import TestCase
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from rest_framework.test import APIRequestFactory
+from rest_framework.viewsets import ModelViewSet
+
+import rules
+from rules.contrib.rest_framework import AutoPermissionViewSetMixin
+
+
+@unittest.skipIf(sys.version_info.major < 3, "Python 3 only")
+class AutoPermissionRequiredMixinTests(TestCase):
+    def setUp(self):
+        from testapp.models import TestModel
+
+        class TestModelSerializer(ModelSerializer):
+            class Meta:
+                model = TestModel
+                fields = "__all__"
+
+        class TestViewSet(AutoPermissionViewSetMixin, ModelViewSet):
+            queryset = TestModel.objects.all()
+            serializer_class = TestModelSerializer
+            permission_type_map = AutoPermissionViewSetMixin.permission_type_map.copy()
+            permission_type_map["custom_detail"] = "add"
+            permission_type_map["custom_nodetail"] = "add"
+
+            @action(detail=True)
+            def custom_detail(self, request):
+                return Response()
+
+            @action(detail=False)
+            def custom_nodetail(self, request):
+                return Response()
+
+            @action(detail=False)
+            def unknown(self, request):
+                return Response()
+
+        self.model = TestModel
+        self.vs = TestViewSet
+        self.req = APIRequestFactory().get("/")
+        self.req.user = AnonymousUser()
+
+    def test_predefined_action(self):
+        # Create should be allowed due to the add permission set on TestModel
+        self.assertEqual(self.vs.as_view({"get": "create"})(self.req).status_code, 201)
+        # List should be allowed due to None in permission_type_map
+        self.assertEqual(
+            self.vs.as_view({"get": "list"})(self.req, pk=1).status_code, 200
+        )
+        # Retrieve should be allowed due to the view permission set on TestModel
+        self.assertEqual(
+            self.vs.as_view({"get": "retrieve"})(self.req, pk=1).status_code, 200
+        )
+        # Destroy should be forbidden due to missing delete permission
+        self.assertEqual(
+            self.vs.as_view({"get": "destroy"})(self.req, pk=1).status_code, 403
+        )
+
+    def test_custom_actions(self):
+        # Both should not produce 403 due to being mapped to the add permission
+        self.assertEqual(
+            self.vs.as_view({"get": "custom_detail"})(self.req, pk=1).status_code, 404
+        )
+        self.assertEqual(
+            self.vs.as_view({"get": "custom_nodetail"})(self.req).status_code, 200
+        )
+
+    def test_unknown_action(self):
+        with self.assertRaises(ImproperlyConfigured):
+            self.vs.as_view({"get": "unknown"})(self.req)
--- tests/testsuite/contrib/test_views.py
+++ tests/testsuite/contrib/test_views.py
@@ -1,12 +1,18 @@
 from __future__ import absolute_import
 
-from django.core.exceptions import ImproperlyConfigured
+import sys
+import unittest
+
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.http import HttpRequest, Http404
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 from django.utils.encoding import force_str
+from django.views.generic import CreateView, View
 
-from rules.contrib.views import objectgetter
+import rules
+from rules.contrib.views import AutoPermissionRequiredMixin, objectgetter
 
 from testapp.models import Book
 
@@ -19,8 +25,8 @@ class FBVDecoratorTests(TestData, TestCase):
         book = Book.objects.get(pk=1)
 
         self.assertEqual(book, objectgetter(Book)(request, pk=1))
-        self.assertEqual(book, objectgetter(Book, attr_name='id')(request, id=1))
-        self.assertEqual(book, objectgetter(Book, field_name='id')(request, pk=1))
+        self.assertEqual(book, objectgetter(Book, attr_name="id")(request, id=1))
+        self.assertEqual(book, objectgetter(Book, field_name="id")(request, pk=1))
 
         with self.assertRaises(ImproperlyConfigured):
             # Raise if no `pk` argument is provided to the view
@@ -28,7 +34,7 @@ class FBVDecoratorTests(TestData, TestCase):
 
         with self.assertRaises(ImproperlyConfigured):
             # Raise if given invalid model lookup field
-            self.assertEqual(book, objectgetter(Book, field_name='foo')(request, pk=1))
+            self.assertEqual(book, objectgetter(Book, field_name="foo")(request, pk=1))
 
         with self.assertRaises(Http404):
             # Raise 404 if no model instance found
@@ -36,111 +42,178 @@ class FBVDecoratorTests(TestData, TestCase):
 
     def test_permission_required(self):
         # Adrian can change his book
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('change_book', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("change_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin can change Adrian's book
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('change_book', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("change_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Adrian can delete his book
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('delete_book', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("delete_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin can *not* create a book
         # Up to Django v2.1, the response was a redirect to login
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('cbv.create_book'))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("cbv.create_book"))
         self.assertIn(response.status_code, [302, 403])
 
         # Martin can *not* delete Adrian's book and is redirected to login
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('delete_book', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("delete_book", args=(1,)))
         self.assertEqual(response.status_code, 302)
 
         # Martin can *not* delete Adrian's book and an PermissionDenied is raised
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('view_that_raises', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("view_that_raises", args=(1,)))
         self.assertEqual(response.status_code, 403)
 
         # Test views that require a list of permissions
 
         # Adrian has both permissions
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('view_with_permission_list', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("view_with_permission_list", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin does not have delete permission
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('view_with_permission_list', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("view_with_permission_list", args=(1,)))
         self.assertEqual(response.status_code, 302)
 
         # Test views that accept a static object as argument
         # fn is passed to has_perm as-is
 
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('view_with_object', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("view_with_object", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('view_with_object', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("view_with_object", args=(1,)))
         self.assertEqual(response.status_code, 302)
 
 
 class CBVMixinTests(TestData, TestCase):
     def test_get_object_error(self):
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
         with self.assertRaises(AttributeError):
-            self.client.get(reverse('cbv.change_book_error', args=(1,)))
+            self.client.get(reverse("cbv.change_book_error", args=(1,)))
 
     def test_permission_required_mixin(self):
         # Adrian can change his book
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('cbv.change_book', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("cbv.change_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin can change Adrian's book
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('cbv.change_book', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("cbv.change_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Adrian can delete his book
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('cbv.delete_book', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("cbv.delete_book", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin can *not* delete Adrian's book
         # Up to Django v2.1, the response was a redirect to login
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('cbv.delete_book', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("cbv.delete_book", args=(1,)))
         self.assertIn(response.status_code, [302, 403])
 
         # Martin can *not* delete Adrian's book and an PermissionDenied is raised
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('cbv.view_that_raises', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("cbv.view_that_raises", args=(1,)))
         self.assertEqual(response.status_code, 403)
 
         # Test views that require a list of permissions
 
         # Adrian has both permissions
-        self.assertTrue(self.client.login(username='adrian', password='secr3t'))
-        response = self.client.get(reverse('cbv.view_with_permission_list', args=(1,)))
+        self.assertTrue(self.client.login(username="adrian", password="secr3t"))
+        response = self.client.get(reverse("cbv.view_with_permission_list", args=(1,)))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(force_str(response.content), 'OK')
+        self.assertEqual(force_str(response.content), "OK")
 
         # Martin does not have delete permission
         # Up to Django v2.1, the response was a redirect to login
-        self.assertTrue(self.client.login(username='martin', password='secr3t'))
-        response = self.client.get(reverse('cbv.view_with_permission_list', args=(1,)))
+        self.assertTrue(self.client.login(username="martin", password="secr3t"))
+        response = self.client.get(reverse("cbv.view_with_permission_list", args=(1,)))
         self.assertIn(response.status_code, [302, 403])
+
+
+@unittest.skipIf(sys.version_info.major < 3, "Python 3 only")
+class AutoPermissionRequiredMixinTests(TestCase):
+    def setUp(self):
+        from testapp.models import TestModel
+
+        self.model = TestModel
+        self.req = RequestFactory().get("/")
+        self.req.user = AnonymousUser()
+
+    def test_predefined_view_type(self):
+        class TestView(AutoPermissionRequiredMixin, CreateView):
+            model = self.model
+            fields = ()
+
+        self.assertEqual(TestView.as_view()(self.req).status_code, 200)
+
+    def test_custom_view_type(self):
+        class CustomViewMixin:
+            pass
+
+        class TestView(AutoPermissionRequiredMixin, CustomViewMixin, CreateView):
+            model = self.model
+            fields = ()
+            permission_type_map = [
+                (CustomViewMixin, "unknown_perm")
+            ] + AutoPermissionRequiredMixin.permission_type_map
+            raise_exception = True
+
+        with self.assertRaises(PermissionDenied):
+            TestView.as_view()(self.req)
+
+    def test_unknown_view_type(self):
+        class TestView(AutoPermissionRequiredMixin, View):
+            pass
+
+        with self.assertRaises(ImproperlyConfigured):
+            TestView.as_view()(self.req)
+
+    def test_overwrite_perm_type(self):
+        class TestView(AutoPermissionRequiredMixin, CreateView):
+            model = self.model
+            fields = ()
+            permission_type = "unknown"
+            raise_exception = True
+
+        with self.assertRaises(PermissionDenied):
+            TestView.as_view()(self.req)
+
+    def test_disable_perm_checking(self):
+        class TestView(AutoPermissionRequiredMixin, CreateView):
+            model = self.model
+            fields = ()
+            permission_type = None
+
+        self.assertEqual(TestView.as_view()(self.req).status_code, 200)
+
+    def test_permission_required_passthrough(self):
+        class TestView(AutoPermissionRequiredMixin, CreateView):
+            model = self.model
+            fields = ()
+            permission_required = "testapp.unknown_perm"
+            raise_exception = True
+
+        with self.assertRaises(PermissionDenied):
+            TestView.as_view()(self.req)
