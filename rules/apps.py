55a7385bdc95f2e4828796c6860391fcb1d89a40
Graham Lyon <graham.lyon@gmail.com>
2021-11-03 23:21:38 +0100

    update AppConfig for Django 4.1

--- rules/apps.py
+++ rules/apps.py
@@ -3,9 +3,12 @@ from django.apps import AppConfig
 
 class RulesConfig(AppConfig):
     name = "rules"
+    default = True
 
 
 class AutodiscoverRulesConfig(RulesConfig):
+    default = False
+
     def ready(self):
         from django.utils.module_loading import autodiscover_modules
 
