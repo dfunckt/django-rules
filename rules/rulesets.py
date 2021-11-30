a2708f431cd45c423e710c16ecd3f509b1b30a9e
Jack Linke <jacklinke@gmail.com>
2021-11-29 21:18:55 -0500

    Update to ensure direct access to RuleSet dictionary works as expected

--- rules/rulesets.py
+++ rules/rulesets.py
@@ -1,15 +1,20 @@
-from .predicates import predicate
+from .predicates import predicate, Predicate
+from inspect import isfunction
 
 
 class RuleSet(dict):
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
     def test_rule(self, name, *args, **kwargs):
-        return name in self and self[name]['pred'].test(*args, **kwargs)
+        # return name in self and self[name]['pred'].test(*args, **kwargs)
+        return name in self and self[name].test(*args, **kwargs)
 
     def rule_exists(self, name):
         return name in self
 
     def rule_verbose_name(self, name):
-        return self[name]['verbose_name'] or name
+        return self["get_%s_display" % name] or name
 
     def add_rule(self, name, pred, verbose_name=None):
         if name in self:
@@ -18,14 +23,48 @@ class RuleSet(dict):
         self[name] = {'pred': predicate(pred), 'verbose_name': verbose_name}
 
     def set_rule(self, name, pred, verbose_name=None):
-        self[name] = {'pred': pred, 'verbose_name': verbose_name}
+        self[name] = {'pred': predicate(pred), 'verbose_name': verbose_name}
 
     def remove_rule(self, name):
         del self[name]
 
-    def __setitem__(self, name, pred_dict):
-        super(RuleSet, self).__setitem__(name, pred_dict)
+    def __setitem__(self, name, pred):
+        if isfunction(pred):
+            # If a function as passed in (as might be done with legacy django-rules)
+            #   convert the value to the dictionary form
+            pred = {'pred': predicate(pred), 'verbose_name': verbose_name}
 
+        if isinstance(pred, dict):
+            # Check if internal pred is already a Predicate, or an basic
+            #   unwrapped function. Wrap as a Predicate if needed
+            if not isinstance(pred['pred'], Predicate):
+                pred['pred'] = predicate(pred['pred'])
+
+        super(RuleSet, self).__setitem__(name, pred)
+
+    def __getitem__(self, name):
+        def _check_name(_name):
+            if (not super(RuleSet, self).__contains__(_name)):
+                raise KeyError("Provided name '`%s`' not found" % _name)
+
+        if name[0] != '_':
+            prefix = "get_"
+            suffix = "_display"
+            if name.startswith(prefix) and name.endswith(suffix):
+                _name = name[len(prefix):-len(suffix)]
+                _check_name(_name)
+                return super(RuleSet, self).__getitem__(_name)['verbose_name']
+
+            _check_name(name)            
+        return super().__getitem__(name)['pred']
+        
+    def __iter__(self):
+        for name in range(len(self)):
+            yield self[name]
+
+    def __reversed__(self):
+        for name in reversed(range(len(self))):
+            yield self[name]
 
 # Shared rule set
 
